9e8899143914ed201ca3a5cd4069694434c64db1
chylinski.t <chylinski.t@outlook.com>
2019-11-24 16:10:55 -0500

    edited js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,16 +31,20 @@ LETS PLAN
 
  // Your web app's Firebase configuration
  var firebaseConfig = {
-    apiKey: "AIzaSyAyeAyFNzctgu4iTvZ0oQaqNmyRvjivPpk",
-    authDomain: "train-schedule-f7941.firebaseapp.com",
-    databaseURL: "https://train-schedule-f7941.firebaseio.com",
-    projectId: "train-schedule-f7941",
-    storageBucket: "train-schedule-f7941.appspot.com",
-    messagingSenderId: "618237921355",
-    appId: "1:618237921355:web:c216b2f27a63b8e47f25ce"
+    apiKey: "AIzaSyAqZNrflv1yu9USlZXzkiybAlT_-cFTuUA",
+    authDomain: "train-schedule-hw-53a0f.firebaseapp.com",
+    databaseURL: "https://train-schedule-hw-53a0f.firebaseio.com",
+    projectId: "train-schedule-hw-53a0f",
+    storageBucket: "train-schedule-hw-53a0f.appspot.com",
+    messagingSenderId: "786492117314",
+    appId: "1:786492117314:web:3e2f837e65c4cbee9cf8db",
+    measurementId: "G-32LNSGDLTN"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  var dataRef = firebase.database();
 
 //display current time
 $(".currentTime").text(moment().format("h:mm A"));
@@ -48,24 +52,54 @@ $(".currentTime").text(moment().format("h:mm A"));
 var currentTime = moment().format("h:mm");
 
 
+//intial values 
+var trainNumber = 0;
+var trainLine = "";
+var trainDest = "";
+var trainDept = 0;
+var trainPlatForm = "";
 
-$("#addTrain").on("click", function () {
+
+$("#addTrain").on("click", function (event) {
+    event.preventDefault();
     console.log("did i get clicked ");
     //store all values
-    var trainNumber = $("#train-number").val().trim();
-    var trainLine = $("#train-line").val().trim();
-    var trainDest = $("#train-destination").val().trim();
-    var trainDept = $("#train-departure").val().trim();
-    var trainPlatForm = $("#train-platform").val().trim();
+     trainNumber = $("#train-number").val().trim();
+     trainLine = $("#train-line").val().trim();
+     trainDest = $("#train-destination").val().trim();
+     trainDept = $("#train-departure").val().trim();
+     trainPlatForm = $("#train-platform").val().trim();
 
-
-    $('#train-schedule-body').append(
-        '<tr>' +
-        '<td' + trainNumber + '</td>' +
-        '<td>' + trainLine + '</td>' +
-        '<td>' + trainDest + '</td>' +
-        '<td>' + trainDept + '</td>' +
-        '<td>' + trainPlatForm + '</td>' +
-        '</tr>'
-    );
+    //code for the push
+    dataRef.ref().push({
+        
+        trainNumber:trainNumber,
+        trainLine: trainLine,
+        trainDest:trainDest,
+        trainDept:trainDept,
+        trainPlatForm:trainPlatForm
+    })
 });
+
+dataRef.ref().on("child_added", function(childSnapshot) {
+
+
+    console.log(childSnapshot.val().trainNumber);
+    console.log(childSnapshot.val().trainLine);
+    console.log(childSnapshot.val().trainDest);
+    console.log(childSnapshot.val().trainDept);
+    console.log(childSnapshot.val().trainPlatForm);
+
+
+   // full list of items to the wall
+$('#train-schedule-body').append(
+    '<tr>' +
+    '<td' + trainNumber + '</td>' +
+    '<td>' + trainLine + '</td>' +
+    '<td>' + trainDest + '</td>' +
+    '<td>' + trainDept + '</td>' +
+    '<td>' + trainPlatForm + '</td>' +
+    '</tr>'
+);
+
+})
