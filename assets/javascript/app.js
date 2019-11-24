45a8275d1659651c2c8470906b98a36ded2da4cc
chylinski.t <chylinski.t@outlook.com>
2019-11-23 22:30:38 -0500

    created vars, created an onclick

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,3 +32,19 @@ LETS PLAN
 $('.currentTime').text(moment().format('h:mm A'));
 
 var currentTime = (moment().format('h:mm'));
+
+
+
+//store all values
+var trainNumber = $('#train-number').val();
+var trainLine = $('#train-line').val();
+var trainDest = $('#train-destination').val();
+var trainDept = $('#train-departure').val();
+var trainPlatForm = $('#train-platform').val();
+
+
+$('#addTrain').on("click",function(){
+    console.log('did i get clicked ' )
+
+    
+})
